f2b5d0dc14fcfbf722cacbdfd2cafc72f1a49477
programmiri <programmiri@gmail.com>
2017-09-23 12:06:32 +0200

    set some things up :cat:

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
+  get 'pages/about'
+  get 'pages/info'
+
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
