c640558583c4cffe1e252c7073cd5383366a3105
programmiri <programmiri@gmail.com>
2017-09-23 15:39:43 +0200

    removed unneccessary ]

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,5 @@
 class IdeasController < ApplicationController
-  before_action :set_idea, only: [:show, :edit, :update, :destroy]]
+  before_action :set_idea, only: [:show, :edit, :update, :destroy]
 
   # GET /ideas
   # GET /ideas.json
