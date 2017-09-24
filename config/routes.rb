ff6ca24a97b48b99107f4037827fa6ca8998d1fe
programmiri <programmiri@gmail.com>
2017-09-24 20:04:55 +0200

    Cleaned up files and SCSS structure. Layout for all cat view.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/info'
 
-  resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
